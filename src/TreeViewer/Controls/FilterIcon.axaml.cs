cfef81cd5c7addb7a67af772ff0329a132733b16
Giorgio Bianchini <giorgio.bianchini@bristol.ac.uk>
2021-04-27 02:44:42 +0100

    Added option to filter modules.

---
+++ src/TreeViewer/Controls/FilterIcon.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TreeViewer
+{
+    public class FilterIcon : UserControl
+    {
+        public FilterIcon()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
