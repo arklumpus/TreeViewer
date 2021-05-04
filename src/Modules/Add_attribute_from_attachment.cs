472cac4b93eed9703634c7b0e350a0cc1e2d4e32
Giorgio Bianchini <giorgio.bianchini@bristol.ac.uk>
2021-05-04 19:09:14 +0100

    Added option to select the attribute that matches the data in the attachment.

--- src/Modules/Add_attribute_from_attachment.cs
+++ src/Modules/Add_attribute_from_attachment.cs
@@ -1,15 +1,17 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using PhyloTree;
 using TreeViewer;
+using PhyloTree.Extensions;
 
 namespace AddAttributeFromAttachment
 {
     /// <summary>
     /// This module can be used to add an attribute to a list of taxa that is loaded as an attachment. This module is best suited for
     /// a "presence-absence" kind of attribute (i.e. if you have a text file with a list of taxa that have a certain feature, and you
-    /// want to convert this to attributes on the tree). For more complex attributes, use the _Parse tip states_ module.
+    /// want to convert this to attributes on the tree). For more complex attributes, use the _Parse node states_ module.
     /// </summary>
     /// 
     /// <description>
@@ -18,8 +20,8 @@ namespace AddAttributeFromAttachment
     /// To use this module, first of all you will have to load a text file containing one taxon name per line as an attachment.
     /// 
     /// This module parses the selected attachment and reads each line of the file. The requested attribute is added to
-    /// all the nodes in the tree whose `Name` corresponds to a line in the file (and/or to their ancestors, based on the
-    /// value selected for the [Apply to](#apply-to) parameter).
+    /// all the nodes in the tree whose [match attribute](#match-attribute) corresponds to a line in the file (and/or to their ancestors,
+    /// based on the value selected for the [Apply to](#apply-to) parameter).
     /// </description>
 
     public static class MyModule
@@ -27,7 +29,7 @@ namespace AddAttributeFromAttachment
         public const string Name = "Add attribute";
         public const string HelpText = "Adds an attribute based on an attachment.";
         public const string Author = "Giorgio Bianchini";
-        public static Version Version = new Version("1.0.0");
+        public static Version Version = new Version("1.1.0");
         public const string Id = "f71a5e60-5e40-4a5e-9795-e5259fb283ab";
         public const ModuleTypes ModuleType = ModuleTypes.FurtherTransformation;
 
@@ -45,6 +47,16 @@ namespace AddAttributeFromAttachment
                 /// </param>
                 ( "Taxon list:", "Attachment:" ),
                 ( "The taxon list should be an attachment with one taxon per line", "Label:[\"Left\",\"Italic\",\"#808080\"]" ),
+
+                /// <param name="Match attribute:">
+                /// This parameter determines the attribute that is matched agains the list of taxa in the attachment.
+                /// </param>
+                ( "Match attribute:", "AttributeSelector:Name" ),
+                
+                /// <param name="Match attribute type:">
+                /// This parameter is used to select the type of the [match attribute](#match-attribute).
+                /// </param>
+                ( "Match attribute type:", "AttributeType:String" ),
                 
                 /// <param name="Apply to:">
                 /// The value selected for this parameter determines to which taxa the attribute is applied.
@@ -112,6 +124,18 @@ namespace AddAttributeFromAttachment
                 }
             }
 
+            if ((string)previousParameterValues["Match attribute:"] != (string)currentParameterValues["Match attribute:"])
+            {
+                string attributeName = (string)currentParameterValues["Match attribute:"];
+
+                string attrType = ((TreeNode)tree).GetAttributeType(attributeName);
+
+                if (!string.IsNullOrEmpty(attrType) && (string)previousParameterValues["Match attribute type:"] == (string)currentParameterValues["Match attribute type:"])
+                {
+                    parametersToChange.Add("Match attribute type:", attrType);
+                }
+            }
+
             Attachment previousAttachment = (Attachment)previousParameterValues["Taxon list:"];
             Attachment newAttachment = (Attachment)currentParameterValues["Taxon list:"];
 
@@ -124,7 +148,7 @@ namespace AddAttributeFromAttachment
 
             if (attachment != null)
             {
-                string[] taxonList = attachment.GetLines();
+                string[] taxonListString = attachment.GetLines();
 
                 string attributeName = (string)parameterValues["Attribute:"];
 
@@ -134,15 +158,52 @@ namespace AddAttributeFromAttachment
 
                 int applyTo = (int)parameterValues["Apply to:"];
 
+                string matchAttribute = (string)parameterValues["Match attribute:"];
+                string matchAttributeType = (string)parameterValues["Match attribute type:"];
+
+                double[] taxonListDouble = null;
+
+                if (matchAttributeType == "Number")
+                {
+                    double elDouble = double.NaN;
+                    taxonListDouble = (from el in taxonListString where double.TryParse(el, System.Globalization.NumberStyles.Any, System.Globalization.CultureInfo.InvariantCulture, out elDouble) && !double.IsNaN(elDouble) select elDouble).ToArray();
+                }
+
                 if (applyTo == 0 || applyTo == 1 || applyTo == 2)
                 {
-                    TreeNode lca = tree.GetLastCommonAncestor(taxonList);
+                    TreeNode lca = null;
+
+                    if (matchAttributeType == "String")
+                    {
+                        lca = GetLCA(tree, taxonListString, matchAttribute);
+                    }
+                    else if (matchAttributeType == "Number")
+                    {
+                        lca = GetLCA(tree, taxonListDouble, matchAttribute);
+                    }
 
                     foreach (TreeNode leaf in tree.GetLeaves())
                     {
                         TreeNode node = leaf;
 
-                        if (taxonList.Contains(node.Name))
+                        bool matches = false;
+
+                        if (matchAttributeType == "String")
+                        {
+                            if (node.Attributes.TryGetValue(matchAttribute, out object attrObject) && attrObject is string matchAttrValue && !string.IsNullOrEmpty(matchAttrValue))
+                            {
+                                matches = taxonListString.Contains(matchAttrValue);
+                            }
+                        }
+                        else if (matchAttributeType == "Number")
+                        {
+                            if (node.Attributes.TryGetValue(matchAttribute, out object attrObject) && attrObject is double matchAttrValue && !double.IsNaN(matchAttrValue))
+                            {
+                                matches = taxonListDouble.Contains(matchAttrValue);
+                            }
+                        }
+
+                        if (matches)
                         {
                             TreeNode targetNode = applyTo == 0 ? node.Parent : applyTo == 1 ? null : lca?.Parent;
 
@@ -178,7 +239,16 @@ namespace AddAttributeFromAttachment
                 }
                 else if (applyTo == 3 || applyTo == 4)
                 {
-                    TreeNode node = tree.GetLastCommonAncestor(taxonList);
+                    TreeNode node = null;
+
+                    if (matchAttributeType == "String")
+                    {
+                        node = GetLCA(tree, taxonListString, matchAttribute);
+                    }
+                    else if (matchAttributeType == "Number")
+                    {
+                        node = GetLCA(tree, taxonListDouble, matchAttribute);
+                    }
 
                     if (node == null)
                     {
@@ -244,5 +314,87 @@ namespace AddAttributeFromAttachment
             }
         }
 
+        public static TreeNode GetLCA(TreeNode tree, string[] taxonList, string attributeName)
+        {
+            List<TreeNode> nodes = tree.GetChildrenRecursive();
+
+            if (taxonList.Length > 0)
+            {
+                TreeNode seed = null;
+
+                foreach (TreeNode node in nodes)
+                {
+                    if (node.Attributes.TryGetValue(attributeName, out object attrValue) && attrValue is string attrString && !string.IsNullOrEmpty(attrString) && taxonList.Contains(attrString))
+                    {
+                        seed = node;
+                        break;
+                    }
+                }
+
+                while (seed != null && !GetAllAttributesString(seed, attributeName).ContainsAll(taxonList))
+                {
+                    seed = seed.Parent;
+                }
+
+                return seed;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static TreeNode GetLCA(TreeNode tree, double[] taxonList, string attributeName)
+        {
+            List<TreeNode> nodes = tree.GetChildrenRecursive();
+
+            if (taxonList.Length > 0)
+            {
+                TreeNode seed = null;
+
+                foreach (TreeNode node in nodes)
+                {
+                    if (node.Attributes.TryGetValue(attributeName, out object attrValue) && attrValue is double attrDouble && !double.IsNaN(attrDouble) && taxonList.Contains(attrDouble))
+                    {
+                        seed = node;
+                        break;
+                    }
+                }
+
+                while (seed != null && !GetAllAttributesDouble(seed, attributeName).ContainsAll(taxonList))
+                {
+                    seed = seed.Parent;
+                }
+
+                return seed;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static IEnumerable<string> GetAllAttributesString(TreeNode node, string attributeName)
+        {
+            foreach (TreeNode child in node.GetChildrenRecursiveLazy())
+            {
+                if (child.Attributes.TryGetValue(attributeName, out object attrValue) && attrValue is string attrString && !string.IsNullOrEmpty(attrString))
+                {
+                    yield return attrString;
+                }
+            }
+        }
+
+        public static IEnumerable<double> GetAllAttributesDouble(TreeNode node, string attributeName)
+        {
+            foreach (TreeNode child in node.GetChildrenRecursiveLazy())
+            {
+                if (child.Attributes.TryGetValue(attributeName, out object attrValue) && attrValue is double attrDouble && !double.IsNaN(attrDouble))
+                {
+                    yield return attrDouble;
+                }
+            }
+        }
     }
 }
+
